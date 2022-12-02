25dfeb532b8172b46f542985a1b32666c9e942e4
MacFab <me@fabianstricker.com>
2022-12-02 17:49:24 +0100

    Changed the app to become a little more respnsive by checking against the user's device width, and changing the node content (x/y information) and layout (orientation = v instead of h)

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
-# %%
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 from functions import *
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
-# import pyautogui
+import pyautogui
 
-# %%
+
+# In[2]:
+
+
 #load and preprocess data
 df = load_data('data', 'EU_flow_data_20220322.csv', wide_boolean=True, idx_boolean=False)
 
-# %%
 #dynamic sankey visualization with radio buttons and slider filters
 app = Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 server = app.server
 
 #shape sankey layout height and orientation depending on user device
-# user_gui_width, user_gui_height = pyautogui.size()[0], pyautogui.size()[1]
-# sankey_orientation = "h"
-# if user_gui_width < 1200:
-#     sankey_orientation = "v"
+user_gui_width, user_gui_height = pyautogui.size()[0], pyautogui.size()[1]
+sankey_orientation = "h"
+if user_gui_width < 900:
+    sankey_orientation = "v"
+
+
+# In[ ]:
 
 
 app.layout = html.Div([
@@ -39,9 +49,8 @@ app.layout = html.Div([
     ], style={'height': '20%'}),
 
     html.Div([
-        dcc.Graph(id='indicator-graphic')
+        dcc.Graph(id='indicator-graphic') # , responsive=True
     ], style={'width': '100%', 'height': '70%'}),
-    #, style={'background-size': '16.7%', 'background-image': 'linear-gradient(to right, #AAA 1px, transparent 1px)'}    
     html.Div([
         dcc.Slider(
             df['Value.info'].min(),
@@ -52,7 +61,7 @@ app.layout = html.Div([
             marks={str(year): str(year) for year in df['Value.info'].unique()},
         )], style={'height': '10%', 'fontFamily': 'Arial', 'width': '70%', 'margin': 'auto'})
     
-], style= {'height': '100%'})
+], style= {'height': '100vh'})
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
@@ -87,23 +96,43 @@ def update_graph_and_title(xaxis_column_name, year_value):
     keys_payload = x_level_strings(x_lvl_dict)
     
     payload_title = f"EU27 material flows in {year_value} for {xaxis_column_name} along {keys_payload}"
+    
+    #ready node data for mobile or larger displays
+    node_config = dict(
+        label = unique_sources_targets,
+        x = [.001 if x==0 else .999 if x == 1 else x for x in payload_x],
+        y = [.001 if y==0 else .999 if y == 1 else y for y in payload_y],
+        pad = 20,
+        thickness = 20,
+        color = "cornsilk"
+    )
+    
+    domain_config = dict(
+        x =  [0,1],
+        y =  [0,1]
+    )
+    
+    if sankey_orientation == "v":
+        
+        node_config = dict(
+#             below manipulation unfortunately doesn't work (node labels are not shown)
+#             label = ['\n' + unique_sources_targets[i] for i in range(len(unique_sources_targets)) if i%2 == 0],
+            label = unique_sources_targets,
+            pad = 20,
+            thickness = 20,
+            color = "cornsilk"
+        )
+#         domain_config = dict(
+#             x =  [0.5,1],
+#             y =  [0.5,1]
+#         )
 
     new_fig = go.Figure(data=[
         go.Sankey(
-            # orientation = sankey_orientation,
-            domain = dict(
-                x =  [0,1],
-                y =  [0,1]
-            ),
+            orientation = sankey_orientation,
+            domain = domain_config,
             arrangement= "snap",
-                node = dict(
-                label = unique_sources_targets,
-                x = [.001 if x==0 else .999 if x == 1 else x for x in payload_x],
-                y = [.001 if y==0 else .999 if y == 1 else y for y in payload_y],
-                pad = 20,
-                thickness = 20,
-                color = "cornsilk"
-            ),
+            node = node_config,
             link = dict(
                 source = source,
                 target = target,
@@ -113,20 +142,19 @@ def update_graph_and_title(xaxis_column_name, year_value):
             )
         )
     ])
-    # new_fig.layout.autosize = True
-    # new_fig.layout.height = 0.7*user_gui_height
-    # new_fig.layout.width = 0.9*user_gui_width
-    # new_fig.update_traces(autosize = 'True')    
-    new_fig.update_layout(height=600)
-    # new_fig.update_traces(orientation=sankey_orientation, selector=dict(type='sankey'))
+    new_fig.update_layout(height = 600)
+#     new_fig.layout.width = 1300 #0.9*user_gui_width 
+#     new_fig.update_layout(height=700)
 
     return new_fig, payload_title
 
-# %%
 # Main
 if __name__ == "__main__":
     app.run_server(debug=False)
-# %%
+
+
+# In[ ]:
+
 
 
 
